c633595703d356d42c28656a490673362823bf2a
julianacrespin <102978768+julianacrespin@users.noreply.github.com>
2022-05-15 20:06:15 -0300

    web

---
+++ web/http&rest_desafios.py
@@ -0,0 +1,29 @@
+import requests
+# para ver todos
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas")
+print(pedido.json()) 
+
+#1
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas/20")
+print(pedido.json())
+
+#2
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas/400")
+print(pedido.json())
+# no hay
+# Para saber cuántos hay hago:
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas")
+print(len(pedido.json()))
+
+#3
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas/20")
+print(pedido.json())
+
+#400
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas/400")
+print(pedido) # me da <Response [404]> que es Error404
+print(pedido.headers) # me dice toda la metadata, toda la información relacionada a ese pedido 
+
+pedido = requests.get("https://macowins-server.herokuapp.com/prendas/20")
+# pedido = requests.get("https://macowins-server.herokuapp.com/prendas")
+print(pedido.status_code) #status code
